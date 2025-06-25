89d53243a290cee34665b3967225b2d1630d06d3
gihyeonchoi <neibie@naver.com>
2025-06-25 15:57:16 +0900

    카드 등록시 page_id 삭제

--- RFID/urls.py
+++ RFID/urls.py
@@ -11,9 +11,12 @@ urlpatterns = [
     # path('card_add/<str:page_id>/', views.card_add, name='card_add'),
     path('test/', views2.card_tag, name='card_add_test'),
 
-    # RFID 레코드 HTML 페이지
-    path('rfid_records/', views2.view_records, name='view_records'),
+
+    
+    # RFID 태그 확인 페이지 (관리자 전용)
+    path('view_tag/', views2.view_tag, name='view_tag'),
     # RFID 레코드 JSON 데이터 (AJAX용)
     path('get_records_json/', views2.get_records_json, name='get_records_json'),
-    path('card_check/<str:page_id>/', views2.card_check, name='card_check'),
+    # RFID 저장 페이지
+    path('card_add/<str:page_id>/', views2.card_add, name='card_add'),
 ]  
